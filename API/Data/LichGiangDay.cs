cec776f40721241bcc9a0d367219c89d83de8514
hungnmph51498 <hungnmph51498.com>
2025-06-26 00:52:11 +0700

    validate & lichhoc

--- API/Data/LichGiangDay.cs
+++ API/Data/LichGiangDay.cs
@@ -7,15 +7,31 @@ namespace API.Data
     {
         [Key]
         public Guid IdLichDay { get; set; } = Guid.NewGuid();
+
+        [Required(ErrorMessage = "IdNXCH là bắt buộc.")]
         public Guid IdNXCH { get; set; }
+
         public bool TTDiemDanhMuon { get; set; } = false;
+
+        [Required(ErrorMessage = "IdNhomXuong là bắt buộc.")]
         public Guid IdNhomXuong { get; set; }
-        public Guid? IdDuAn { get; set; }
+
+        public Guid? IdDuAn { get; set; } // Có thể null
+
+        [Required(ErrorMessage = "IdDiaDiem là bắt buộc.")]
         public Guid IdDiaDiem { get; set; }
+
+        [Required(ErrorMessage = "Hình thức giảng dạy là bắt buộc.")]
+        [StringLength(100, ErrorMessage = "Hình thức giảng dạy không được vượt quá 100 ký tự.")]
         public string HTGiangDay { get; set; }
+
         public bool TTDiemDanh { get; set; } = false;
+
         public DateTime NgayTao { get; set; } = DateTime.Now;
+
         public DateTime? NgayCapNhat { get; set; }
+
+        [Range(0, 2, ErrorMessage = "Trạng thái chỉ nhận giá trị từ 0 đến 2.")]
         public int TrangThai { get; set; } = 1;
 
         // Navigation properties
--- API/Data/LichHoc.cs
+++ API/Data/LichHoc.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Data
 {
@@ -6,14 +8,26 @@ namespace API.Data
     {
         [Key]
         public Guid IDLichHoc { get; set; }
+
+        [Required(ErrorMessage = "IdNXCH là bắt buộc.")]
         public Guid IdNXCH { get; set; }
+
+        [Required(ErrorMessage = "IdNhomXuong là bắt buộc.")]
         public Guid IdNhomXuong { get; set; }
+
+        [Required(ErrorMessage = "IDHocKy là bắt buộc.")]
         public Guid IDHocKy { get; set; }
         public int TrangThai { get; set; }
 
+        [ForeignKey("IdNXCH")]
         public KHNXCaHoc KHNXCaHoc { get; set; }
+
+        [ForeignKey("IDHocKy")]
         public HocKy HocKy { get; set; }
+
+        [ForeignKey("IdNhomXuong")]
         public NhomXuong NhomXuong { get; set; }
+
         public DuAn DuAn { get; set; }
     }
 }
