394566b4c15c9a2f2a724e6d6ec1758b8b30077f
hungnmph51498 <hungnmph51498@gmail.com>
2025-07-13 22:28:16 +0700

    Bỏ Học kỳ, Kế hoạch-Thêm Giảng viên, Địa điểm

--- API/Controllers/LichHocsController.cs
+++ API/Controllers/LichHocsController.cs
@@ -16,7 +16,6 @@ public class LichHocViewsController : ControllerBase
     [HttpGet("sinhvien/{idSinhVien}")]
     public async Task<IActionResult> GetLichHocTheoSinhVien(Guid idSinhVien)
     {
-        // Lấy nhóm xưởng của sinh viên
         var sinhVien = await _context.SinhViens
             .AsNoTracking()
             .FirstOrDefaultAsync(sv => sv.IdSinhVien == idSinhVien);
@@ -26,41 +25,44 @@ public class LichHocViewsController : ControllerBase
 
         Guid idNhomXuong = sinhVien.IdNhomXuong.Value;
 
-        // Truy vấn lịch học thuộc nhóm xưởng của sinh viên
         var data = await (from khnxch in _context.KHNXCaHocs
                           join khnx in _context.KeHoachNhomXuongs on khnxch.IdKHNX equals khnx.IdKHNX
                           join kh in _context.KeHoachs on khnx.IdKeHoach equals kh.IdKeHoach
                           join nx in _context.NhomXuongs on khnx.IdNhomXuong equals nx.IdNhomXuong
                           join da in _context.DuAns on kh.IdDuAn equals da.IdDuAn
-                          join hk in _context.HocKys on da.IdHocKy equals hk.IdHocKy
-                          where nx.IdNhomXuong == idNhomXuong // Lọc theo nhóm xưởng của SV
+                          join lgd in _context.LichGiangDays on khnxch.IdNXCH equals lgd.IdNXCH
+                          join dd in _context.DiaDiems on lgd.IdDiaDiem equals dd.IdDiaDiem
+                          join gv in _context.PhuTrachXuongs on nx.IdPhuTrachXuong equals gv.IdNhanVien
+                          where nx.IdNhomXuong == idNhomXuong
                           select new LichHocViewDto
                           {
-                              TenHocKy = hk.TenHocKy,
                               TenDuAn = da.TenDuAn,
-                              TenKeHoach = kh.TenKeHoach,
                               TenNhomXuong = nx.TenNhomXuong,
                               NgayHoc = khnxch.NgayHoc,
                               Buoi = khnxch.Buoi,
                               ThoiGian = khnxch.ThoiGian,
                               NoiDung = khnxch.NoiDung,
-                              LinkOnline = khnxch.LinkOnline
+                              LinkOnline = khnxch.LinkOnline,
+                              TenGiangVien = gv.TenNhanVien,
+                              TenDiaDiem = dd.TenDiaDiem
                           }).ToListAsync();
 
         return Ok(data);
     }
 
 
+
     public class LichHocViewDto
     {
-        public string TenHocKy { get; set; }
         public string TenDuAn { get; set; }
-        public string TenKeHoach { get; set; }
         public string TenNhomXuong { get; set; }
         public DateTime NgayHoc { get; set; }
         public string Buoi { get; set; }
         public string ThoiGian { get; set; }
         public string NoiDung { get; set; }
         public string LinkOnline { get; set; }
+        public string TenGiangVien { get; set; }     // thêm
+        public string TenDiaDiem { get; set; }       // thêm
     }
+
 }
